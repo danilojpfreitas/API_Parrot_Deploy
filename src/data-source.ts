9e7584f16df1362464defdfb0ff21e3cd7f40897
danilojpfreitas <danilojpfreitas@gmail.com>
2022-09-20 17:15:18 -0300

    Deploy

--- src/data-source.ts
+++ src/data-source.ts
@@ -7,7 +7,7 @@ const port = process.env.DB_PORT as number | undefined
 export const AppDataSource = new DataSource({
     type: 'mysql',
     host: process.env.DB_HOST,
-    port: port,
+    port: 3306,
     username: process.env.DB_USER,
     password: process.env.DB_PASS,
     database: process.env.DB_NAME,
